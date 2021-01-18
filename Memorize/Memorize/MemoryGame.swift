bbfe7739f0b2e9ddb3c6acf2132ba8faecbdc83a
Amr Hesham <amrhesham9595@gmail.com>
2021-01-18 22:43:15 +0200

    MVVM applied

---
+++ Memorize/Memorize/MemoryGame.swift
@@ -0,0 +1,33 @@
+//
+//  MemoryGame.swift
+//  Memorize
+//
+//  Created by Amr Hesham on 18/01/2021.
+//
+
+import Foundation
+
+struct MemoryGame<CardContent> {
+    var cards: Array<Card>
+    
+    init(numberOfPairs: Int, cardContentFactory: (Int) -> CardContent) {
+        cards = Array<Card>()
+        for pairIndex in 0..<numberOfPairs {
+            let content = cardContentFactory(pairIndex)
+            cards.append(Card(content: content, id: pairIndex*2))
+            cards.append(Card(content: content, id: pairIndex*2+1))
+
+        }
+    }
+    
+    func choose(card: Card) {
+        print("card chosen: \(card)")
+    }
+    
+    struct Card: Identifiable {
+        var isFaceUp: Bool = true
+        var isMatched: Bool = false
+        var content: CardContent
+        var id: Int
+    }
+}
